5c99137b31086bc9991f07d2502dfc6d5b478a52
Amit Lissack <amit.lissack@aprime.io>
2023-06-17 22:39:34 -0400

    from_spa,to_spa

--- rtx/util.py
+++ rtx/util.py
@@ -1,9 +1,9 @@
-def convert(command: int, arinc_word: int) -> int:
-    """Convert an Arinc 29 32-bit Word to an RTX-SPA ready 40-bit integer.
+def to_rtx_spa_command(command: int, arinc_word: int) -> int:
+    """Convert an Arinc 29 32-bit Word to an RTX-SPA ready 40-bit command.
 
     :param command:
     :param arinc_word: Arinc 29 word
-    :return: RTX-SPA 40 bit integer
+    :return: RTX-SPA 40 bit command
     """
     # Bit 32 is always 1.
     result = 0x0100000000 | ((command & 7) << 33)
@@ -18,3 +18,18 @@ def convert(command: int, arinc_word: int) -> int:
     # Bits 6-0
     result |= (arinc_word & 0x0000007F) << 1
     return result
+
+
+def from_rtx_spa_command(rtx_spa_command: int) -> tuple[int, int]:
+    """Convert RTX spa 40 bit command into a command and Arinc29 32 bit word
+
+    :param rtx_spa_command: The rtx spa command
+    :return: A tuple of command and arinc word.
+    """
+    command = (rtx_spa_command >> 33) & 0x7
+    arinc_word = (rtx_spa_command & 0xF000000000) >> 8
+    arinc_word |= (rtx_spa_command & 0xFE000000) >> 4
+    arinc_word |= (rtx_spa_command & 0xFE0000) >> 3
+    arinc_word |= (rtx_spa_command & 0xFE00) >> 2
+    arinc_word |= (rtx_spa_command & 0xFE) >> 1
+    return command, arinc_word
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,9 @@
-import pytest
-from rtx.util import convert
+
+from rtx import util
+
+def test_to_rtx_spa_command() -> None:
+    assert util.to_rtx_spa_command(1, 0x12345678) == 0x1322A258F0
 
 
-def test_convert() -> None:
-    assert convert(1, 0x12345678) == 0x1322A258F0
+def test_from_rtx_spa_command() -> None:
+    assert util.from_rtx_spa_command(0x1322A258F0) == (1, 0x12345678)
