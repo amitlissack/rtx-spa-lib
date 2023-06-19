ae4d2a79bf198bc8b96671ab2d8da1cac904ccbf
Amit Lissack <amit.lissack@aprime.io>
2023-06-19 11:45:50 -0400

    enable flow control

---
+++ tests/commands/test_enable_flow_control.py
@@ -0,0 +1,13 @@
+import pytest
+
+from rtx.command.flow_control import RtxEnableFlowControlCommand
+
+
+@pytest.fixture
+def subject() -> RtxEnableFlowControlCommand:
+    return RtxEnableFlowControlCommand()
+
+
+def test_enable_flow_control(subject: RtxEnableFlowControlCommand) -> None:
+    """It should create the command correctly."""
+    assert subject.get_bytes() == bytes([0x15, 0x15])
